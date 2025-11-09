d21b5af2fe8907fd28b7f755c78ba14d141fac7f
Hariprakash3131 <168099983+Hariprakash3131@users.noreply.github.com>
2025-11-09 23:00:13 +0530

    Remove end and error event listeners from stream
    Removed 'end' and 'error' event listeners from the readable stream.

--- Sample.js
+++ Sample.js
@@ -12,10 +12,3 @@ readableStream.on('data', (chunk) => {
   console.log(chunk);
 });
 
-readableStream.on('end', () => {
-  console.log('No more data to read.');
-});
-
-readableStream.on('error', (err) => {
-  console.error('Error reading from stream:', err);
-});
